7a0437b53ab855262057b563ba68e22ddf02ae0c
binna <binna@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2011-10-21 11:55:28 +0000

    add example test
    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@3 b0e66d11-08a8-0410-84da-a809a5ca4a2f

---
+++ src/test/java/at/ac/dbisinformatik/snowprofile/ExampleTest.java
@@ -0,0 +1,15 @@
+package at.ac.dbisinformatik.snowprofile;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Robert Binna
+ *
+ */
+public class ExampleTest {
+	@Test
+	public void testOk() {
+		Assert.assertTrue(true);
+	}
+}

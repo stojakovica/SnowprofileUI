a94aa3c89fcfc6e1c978833da8eb643576aa86dc
aleksandar.stojakovic <aleksandar.stojakovic@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-02-20 20:58:27 +0000

    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@20 b0e66d11-08a8-0410-84da-a809a5ca4a2f

--- src/test/java/at/ac/dbisinformatik/snowprofile/dataconverter/ConverterTest.java
+++ src/test/java/at/ac/dbisinformatik/snowprofile/dataconverter/ConverterTest.java
@@ -1,12 +1,8 @@
 package at.ac.dbisinformatik.snowprofile.dataconverter;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Scanner;
 

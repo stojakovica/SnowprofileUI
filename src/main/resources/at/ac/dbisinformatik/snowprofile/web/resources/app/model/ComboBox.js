d8042ddf4da5ddc200162b64b172f712226e7dbd
aleksandar.stojakovic <aleksandar.stojakovic@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-03-19 22:19:23 +0000

    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@32 b0e66d11-08a8-0410-84da-a809a5ca4a2f

---
+++ src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/model/ComboBox.js
@@ -0,0 +1,8 @@
+Ext.define('LWD.model.ComboBox', {
+	extend: 'Ext.data.Model',
+	fields: [
+	    {type: 'string', name: 'abbr'},
+	    {type: 'string', name: 'name'},
+ 		{type: 'string', name: 'slogan'}
+ 	]
+});
--- src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app_new.js
+++ src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app_new.js
@@ -1,3 +1,7 @@
+Ext.require([ 
+    'Ext.layout.container.*', 
+]);
+
 Ext.application({
     name: 'LWD',
 
@@ -14,10 +18,87 @@ Ext.application({
     launch: function() {
         Ext.create('Ext.Viewport', {
         	layout:'fit',
-        	items: {
-        	    xtype: 'menu',
-        	    border: false
-        	}
+        	items: [ 
+        		{
+        			xtype: 'panel',
+                    items: [
+                        {
+                        	xtype: 'menu',
+                        	border: false
+                        },
+                        {
+                        	xtype: 'panel',
+                        	layout: 'column',
+                            defaults: {
+                                layout: 'anchor',
+                                defaults: {
+                                    anchor: '100%',
+                                    collapsible: true
+                                }
+                            },
+                	    	items: [{
+                                columnWidth: 1/2,
+                                baseCls:'x-plain',
+                                items: [
+                                    {
+                                    	xtype: 'tabpanel',
+                                    	activeTab: 0, // index or id
+                                    	height: '50%',
+                                    	items:[
+                                    	    {
+	                                    		title: 'Kopf',
+	                                    		autoScroll: true,
+	                                    		bodyStyle: 'padding:5px; border:none',
+	                                    		items: [
+	                                		        {
+	                                		        	xtype: 'eingabeform',
+	                                		        	bodyStyle: 'border:none',
+	                                		        	border: false
+	                                		        }
+	                                    		]
+	                                    	},
+	                                    	{
+	                                    	    title: 'Karte',
+	                                    	    items: [
+                                    	            {
+                                    	            	xtype: 'googlemaps',
+                                    	            	height: '100%',
+                                    	            }
+                                   	            ]
+	                                    	}
+                                    	]
+                                    },
+                                    {
+                                    	xtype: 'panel',
+                                    	layout: 'fit',
+                                    	height: '50%',
+                                    	items: [
+                                	        {
+                                	        	xtype: 'regionen',
+                                	        	border: true,
+                                	        	autoScroll: true
+                                	        }
+                                    	]
+                                    }
+                                ]
+                            },{
+                                columnWidth: 1/2,
+                                baseCls:'x-plain',
+                                items: [
+                                    {
+                                    	xtype: 'panel',
+                                    	layout: 'fit',
+                                    	height: '100%',
+                                    	autoScroll: true,
+                                    	border: true,
+                                    	html: '<img src="/lwd/static/1.0.0.0/data/img/test.gif" title="Test-Bild" style="width:100%" />'
+                                    }
+                                ]
+                            }]
+                        }
+                    ]
+        		}
+        	]
         });
     }
     

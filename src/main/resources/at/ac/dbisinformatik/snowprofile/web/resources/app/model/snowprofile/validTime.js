93a3d5e1fa7c135b11084e8cd58dc630d449a9c5
aleksandar.stojakovic <aleksandar.stojakovic@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-06-19 16:26:27 +0000

    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@47 b0e66d11-08a8-0410-84da-a809a5ca4a2f

--- src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/model/snowprofile/validTime.js
+++ src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/model/snowprofile/validTime.js
@@ -1,6 +1,7 @@
 Ext.define('LWD.model.snowprofile.validTime', {
 	extend: 'Ext.data.Model',
-    fields: ['id', 'timePosition'],
-    
+    associations: [
+       {type: 'hasMany', model: 'LWD.model.snowprofile.TimeInstant', name: 'TimeInstant', associationKey:'TimeInstant'}
+    ],
     belongsTo: 'LWD.model.Snowprofile'
 });

1b7ea607f5faebf542ab139b54fdb581d6c1f1bf
aleksandar.stojakovic <aleksandar.stojakovic@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-08-22 17:34:32 +0000

    correct snowTemp view
    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@69 b0e66d11-08a8-0410-84da-a809a5ca4a2f

--- src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/model/TempProfile.js
+++ src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/model/TempProfile.js
@@ -2,6 +2,10 @@ Ext.define('LWD.model.TempProfile', {
 	extend: 'Ext.data.Model',
 	fields: [
 	    { mapping: "depth", name:"depth" },
-	    { mapping: "snowTemp", name:"snowTemp" }
+	    { mapping: "snowTemp", name:"snowTemp" },
+	    { name:"temp", convert: function(v, record) {
+	    	return record.data.snowTemp/10;
+	      } 
+	    }
 	]
 });

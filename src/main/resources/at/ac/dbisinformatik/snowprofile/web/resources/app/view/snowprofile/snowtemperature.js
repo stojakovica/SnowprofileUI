4317d09d73b45c85d5768404ac5734ca1a0a559d
aleksandar.stojakovic <aleksandar.stojakovic@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-08-22 17:33:01 +0000

    clean comments from snowtemperature
    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@68 b0e66d11-08a8-0410-84da-a809a5ca4a2f

--- src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/view/snowprofile/snowtemperature.js
+++ src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/app/view/snowprofile/snowtemperature.js
@@ -48,7 +48,7 @@ Ext.define('LWD.view.snowprofile.snowtemperature' ,{
 		},
 		{
 			header: 'Temperatur[°C]',
-			dataIndex: 'snowTemp',
+			dataIndex: 'temp',
 			flex: 1,
 			editor: {
 				xtype: 'numberfield',
@@ -59,107 +59,6 @@ Ext.define('LWD.view.snowprofile.snowtemperature' ,{
 	],
 	
     initComponent: function() {
-    	/*this.columns = [
-			{
-				header: 'Von Höhe[cm]',
-				dataIndex: 'depthTop',
-				flex: 1,
-				editor: {
-				    xtype: 'numberfield',
-	                allowBlank: false,
-	                minValue: 0,
-	                maxValue: 700
-				}
-			},
-			{
-				header: 'Bis Höhe[cm]',
-				dataIndex: 'depthTop',
-				flex: 1,
-				editor: {
-					xtype: 'numberfield',
-	                allowBlank: false,
-	                minValue: 0,
-	                maxValue: 700
-				}
-			},
-			{
-				header: 'Kornform[F]',
-				dataIndex: 'kornform',
-		        width: 130,
-	            editor: new Ext.form.field.ComboBox({
-	                typeAhead: true,
-	                triggerAction: 'all',
-	                selectOnTab: true,
-	                store: [
-	                    ['1-1-1','1-1-1'],
-	                    ['2-2-2','2-2-2'],
-	                    ['3-3-3','3-3-3'],
-	                    ['4-4-4','4-4-4'],
-	                    ['6-4-4','6-4-4'],
-	                    ['6-6-6','6-6-6']
-	                ],
-	                lazyRender: true,
-	                listClass: 'x-combo-list-small'
-	            })
-			},
-			{
-	            header: 'Grösse[D][mm]',
-	            dataIndex: 'groesse',
-	            flex: 1,
-	            editor: {
-	                allowBlank: false
-	            }
-			},
-			{
-				header: 'Härte[K]',
-				dataIndex: 'haerte',
-		        width: 130,
-	            editor: new Ext.form.field.ComboBox({
-	                typeAhead: true,
-	                triggerAction: 'all',
-	                selectOnTab: true,
-	                store: [
-	                    ['1','1 (FA - sehr schwach)'],
-	                    ['1-2','1-2'],
-	                    ['2','2 (4F - weich)'],
-	                    ['2-3','2-3'],
-	                    ['3','3 (1F - mittelhart)'],
-	                    ['3-4','3-4'],
-	                    ['4','4 (B - hart)'],
-	                    ['4-5','4-5'],
-	                    ['5','5 (M - sehr hart)'],
-	                    ['6','6 (Eis - kompakt)']
-	                ],
-	                lazyRender: true,
-	                listClass: 'x-combo-list-small'
-	            })
-			},
-			{
-				header: 'Feuchte',
-				dataIndex: 'feuchte',
-				width: 130,
-				editor: new Ext.form.field.ComboBox({
-					typeAhead: true,
-					triggerAction: 'all',
-					selectOnTab: true,
-					store: [
-				        ['1','(1) trocken'],
-				        ['2','(2) schwach feucht'],
-				        ['3','(3) feucht'],
-				        ['4','(4) nass'],
-				        ['5','(5) sehr nass']
-	        		],
-	        		lazyRender: true,
-	        		listClass: 'x-combo-list-small'
-				})
-			}
-			
-        ];*/
-    	
-        /*this.getSelectionModel().on('selectionchange', function(selModel, selections){
-        	//this.down('#delete').setDisabled(selections.length === 0);
-        });*/
-        
         this.callParent(arguments);
     }
 });

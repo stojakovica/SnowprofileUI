3c177a5f39ca0ecec11cb588459eee30d6b9fb40
aleksandar.stojakovic <aleksandar.stojakovic@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-08-29 16:28:35 +0000

    possible to draw rect as SVG/PDF
    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@83 b0e66d11-08a8-0410-84da-a809a5ca4a2f

--- src/main/java/at/ac/dbisinformatik/snowprofile/web/svgcreator/Test.java
+++ src/main/java/at/ac/dbisinformatik/snowprofile/web/svgcreator/Test.java
@@ -33,31 +33,15 @@ public class Test {
             Scriptable scope = cx.initStandardObjects();  
 //            Reader script = new InputStreamReader(Test.class.getResourceAsStream("/at/ac/dbisinformatik/snowprofile/web/resources/includeFunctions.js"));
             Reader script = new InputStreamReader(Test.class.getResourceAsStream("/at/ac/dbisinformatik/snowprofile/web/resources/data/svgcreator/test.js"));
-            //Reader jsonScriptReader = new InputStreamReader(Test.class.getResourceAsStream("/json.js"));
-            //cx.evaluateReader(scope, jsonScriptReader,"<cmd>", 1, null);
             cx.evaluateReader(scope, script,"<cmd>", 1, null);
             Object func = scope.get("getJSON", scope);
-//            Object func = scope.get("getJSON", scope);
             Object stringify = ((Scriptable) scope.get("JSON", scope)).get("stringify", scope);
             if (func instanceof Function) {
                 Object funcArgs[] = null;
                 Object result = ((Function)func).call(cx, scope, scope, funcArgs);
-                System.out.println(result.getClass().getName());
-                System.out.println(result);
                 String jsonString = (String) ((Function)stringify).call(cx, scope, scope, new Object[] { result });
                 JsonObject jsonObject = (JsonObject) new JsonParser().parse(jsonString);
                 SVGCreator.svgDocument(jsonObject);
-                /*HashMap<String, String> mapParams = new HashMap<String, String>();
-
-                if(obj != null) {
-                    Object[] propIds = NativeObject.getPropertyIds(obj);
-                    for(Object propId: propIds) {
-                        String key = propId.toString();
-                        String value = NativeObject.getProperty(obj, key).toString();
-                        mapParams.put(key, value);
-                    }
-                }*/
-                //work with mapParams next..
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
--- src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/data/svgcreator/test.js
+++ src/main/resources/at/ac/dbisinformatik/snowprofile/web/resources/data/svgcreator/test.js
@@ -1,67 +1,69 @@
 function getJSON()  {
 	return {
-	gradients: [{
-		id: 'grad1',
-		angle: 100,
-		stops: {
-		0: {
-		color: '#AACE36'
-	},
-	100: {
-		color: '#2FA042'
-	}
-	}
-	}, {
-		id: 'grad2',
-		angle: 21,
-		stops: {
-		0: {
-		color: '#79A933'
-	},
-	13: {
-		color: '#70A333'
-	},
-	34: {
-		color: '#559332'
-	},
-	58: {
-		color: '#277B2F'
-	},
-	86: {
-		color: '#005F27'
-	},
-	100: {
-		color: '#005020'
-	}
-	}
-	}, {
-		id: 'grad3',
-		angle: 55,
-		stops: {
-		0: {
-		color: '#79AB35'
-	},
-	53: {
-		color: '#7CBA3D'
-	},
-	100: {
-		color: '#00AA4B'
-	}
-	}
-	}],
 	items: [
-        {
-        	type: "rect",
-        	width: "70%",
-        	height: "99%",
-        	x: "15%",
-        	y: "1%",
-        	"stroke-width": 1,
-        	stroke:"#000000",
-        	fill:"#ffffff",
-        	group: 'rectangles'
-        }]
+		drawRectangle("70%", "99%", "15%", "1%", 1),
+		drawRectangle("70%", "5%", "15%", "1%", 1),
+		drawRectangle("70%", "10%", "15%", "6%", 1),
+		drawRectangle("3%", "10%", "55%", "6%", 1),
+		drawRectangle("3%", "84%", "55%", "16%", 1),
+		drawRectangle("3%", "10%", "70%", "6%", 1),
+		drawRectangle("3%", "84%", "70%", "16%", 1),
+		drawRectangle("40%", "0.5", "15%", "9%", "0.25"),
+		drawRectangle("0.5", "91%", "64%", "10%", "0.25"),
+		drawRectangle("0.5", "86%", "24.25%", "15%", "0.25"),
+		drawRectangle("0.5", "86%", "35%", "15%", "0.25"),
+		drawRectangle("0.5", "86%", "45%", "15%", "0.25"),
+		drawRectangle("0.5", "86%", "52%", "15%", "0.25"),
+		drawRectangle("0.5", "86%", "53.9%", "15%", "0.25"),
+		drawText("Neuschnee", "17%", "2.3%", 0),
+		drawText("Schwimmschnee", "17%", "4.7%", 0),
+		drawText("filzig", "30%", "2.3%", 0),
+		drawText("Schmelzform", "30%", "4.7%", 0),
+		drawText("rundkörnig", "43%", "2.3%", 0),
+		drawText("Oberflächenreif", "43%", "4.7%", 0),
+		drawText("kantig", "57%", "2.3%", 0),
+		drawText("Eislamelle", "57%", "4.7%", 0),
+		drawText("Graupel", "71%", "2.3%", 0),
+		drawText("Feuchte", "70%", "11%", 270),
+		drawText("Kristalle", "62.5%", "7.5%", 0),
+		drawText("Form", "60%", "14%", 0),
+		drawText("Durchm.", "65.5%", "14%", 0),    
+		drawText("Rutschblock", "77%", "14%", 0),       
+		drawText("M", "23.95%", "14%", 270),
+		drawText("B", "34.75%", "14%", 270),
+		drawText("1F", "44.65%", "14%", 270),
+		drawText("4F", "51.65%", "14%", 270),
+		drawText("FA", "53.5%", "14%", 270)
+        ]
 	};
+}
+
+function drawText(text, x, y, rotate) {
+	if(rotate > 0) {
+		return {
+			type: 'text',
+	        text: text,
+	        fill: '#000',
+	        font: '8px Arial',
+	        x: x,
+	        y: y,
+	        rotate: {
+                degrees: 270
+            },
+	        group: 'text'
+		}
+	}
+	else {
+		return {
+			type: 'text',
+			text: text,
+			fill: '#000',
+			font: '8px Arial',
+			x: x,
+			y: y,
+			group: 'text'
+		}
+	}
 }
 
 function drawRectangle(width, height, x, y, stroke_width) {

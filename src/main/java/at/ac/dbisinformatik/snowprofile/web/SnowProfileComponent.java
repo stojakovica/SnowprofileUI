d0ef7a27450583eb4ec8c0b2e14bdce2a41aca27
binna <binna@b0e66d11-08a8-0410-84da-a809a5ca4a2f>
2012-10-18 12:14:00 +0000

    added router
    git-svn-id: https://dbis-informatik.uibk.ac.at/svn/snowprofile/trunk/code@140 b0e66d11-08a8-0410-84da-a809a5ca4a2f

---
+++ src/main/java/at/ac/dbisinformatik/snowprofile/web/SnowProfileComponent.java
@@ -0,0 +1,27 @@
+package at.ac.dbisinformatik.snowprofile.web;
+
+import org.restlet.Application;
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+
+public class SnowProfileComponent extends Component {
+
+	public SnowProfileComponent(Application application) {
+		// Create a new Component.
+
+		// Add a new HTTP server listening on port 8080.
+		getServers().add(Protocol.HTTP, 8080);
+
+		// Define CLAP-Protokoll
+		getClients().add(Protocol.CLAP);
+		getClients().add(Protocol.FILE);
+
+		// Attach the sample application.
+		getDefaultHost().attach("/lwd", application);
+		// server.getContext().getParameters().add("useForwardedForHeader",
+		// "true");
+
+		// Start the component.
+		
+	}
+}
